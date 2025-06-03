df5431e60ca9c96dafd26e13dfa12ec3c07fbc66
Joglekar, Aarav Manish <ajoglekar32@gatech.edu>
2025-06-02 21:39:52 -0700

    types and how to cast to float

--- pythonNew/hello.py
+++ pythonNew/hello.py
@@ -1,5 +1,10 @@
-"""
-this is comment that is written in multiple lines
-"""
-if 5 > 2:
-    print("Hi I hate people")
+x = 5
+y = "69"
+print(x)
+print(y)
+print(type(x))
+print(type(y))
+x = float(5)
+y = float(69)
+print(x)
+print(y)
