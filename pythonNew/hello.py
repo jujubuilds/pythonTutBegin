28c88f8deeebd84f10c3a232cd866ae6865ecba3
Joglekar, Aarav Manish <ajoglekar32@gatech.edu>
2025-06-02 21:50:50 -0700

    global variables

--- pythonNew/hello.py
+++ pythonNew/hello.py
@@ -1,10 +1,8 @@
-x = 5
-y = "69"
-print(x)
-print(y)
-print(type(x))
-print(type(y))
-x = float(5)
-y = float(69)
-print(x)
-print(y)
+global_var = 69
+
+def my_function(): 
+    print("Aarav the goat")
+    print(global_var)#global var can be used anywhere 
+
+
+my_function()
